839980b1ef2645f502c6f4900b2ccf9aad973e5c
rishabh219302118 <98849849+rishabh219302118@users.noreply.github.com>
2024-02-15 05:27:14 +0530

    Update benchscript.js

--- benchscript.js
+++ benchscript.js
@@ -13,10 +13,8 @@ function updateStorage() {
 
 createBtn.addEventListener("click", ()=>{
     let inputBox = document.createElement("p");
-    let img = document.createElement("img");
     inputBox.className = "input-box";
     inputBox.setAttribute("contenteditable", "true");
-    img.src = "delete.png";
     notesContainer.appendChild(inputBox).appendChild(img);
     })
 
